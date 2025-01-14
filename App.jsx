cacb93ae87e21128f0866805e447aa83fe87bb49
ziasarwar93 <engrehman.93@gmail.com>
2025-01-14 13:04:03 +0500

    Add New Components

--- App.jsx
+++ App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
+import First from "./src/components/First";
 
 const App = () => {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>React Native Mobile App Development</Text>
       <Text style={{ fontSize: 30 }}>Zia Sarwar</Text>
-      <Button title="Press Here"/>
+      <Button title="Press Here" />
+      <First/>
+      <First/>
     </View>
-  )
-}
+  );
+};
+
+// const First = () => {
+//   return (
+//     <View>
+//     <Text style={{fontSize :20}}>React Native</Text>
+//     </View>
+//   );
+// };
+
+
 export default App;
---
+++ src/components/First.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const First = () => {
+    return (
+        <View>
+            <Text style={{ fontSize: 20 }}>React Native Tutorial</Text>
+        </View>
+    );
+};
+
+export default First;
