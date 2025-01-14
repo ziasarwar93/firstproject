5eaf8d2a39e440bb4e56f3057f0236596248ead3
ziasarwar93 <engrehman.93@gmail.com>
2025-01-14 12:13:24 +0500

    Text,View,Button

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>React Native Mobile App Development</Text>
+      <Text style={{ fontSize: 30 }}>Zia Sarwar</Text>
+      <Button title="Press Here"/>
+    </View>
+  )
+}
+export default App;
